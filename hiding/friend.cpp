705c869980dd6ee93d7ab816d9f34d21fc41ef24
gongwenbo <gongwenbotarsbot.gmail.com>
2017-10-06 14:21:12 +0800

    add friend function about complete class

--- hiding/friend.cpp
+++ hiding/friend.cpp
@@ -1,3 +1,8 @@
+/*
+friend 使用
+参考：http://blog.csdn.net/jackychu/article/details/3020866
+designer:wenboGong
+*/
 #include"common.h"
 class abb;
 //int abb::get(ab obj);
@@ -10,6 +15,7 @@ class ab{
 	friend void display(ab obj);
 	friend void allDisplay(ab obj1,abb obj2);
 	int get(abb obj);
+	friend class abb;//友元类定义
 };
 
 
@@ -26,7 +32,8 @@ class abb{
 	abb();
 	friend void allDisplay(ab obj1,abb obj2);
 	int get(ab obj);
-	friend int get(abb obj);
+	friend int ab::get(abb obj);//友元类内部函数
+	void getAbFormAbb(ab ojb);
 };
 
 abb::abb(){a=11,b=22;};
@@ -37,11 +44,18 @@ void allDisplay(ab obj1,abb obj2){
 int ab::get(abb obj){
     return obj.b;
  };
+
+void abb::getAbFormAbb(ab ojb){
+
+	std::cout<<ojb.a<<std::endl;
+}
 int main(void){
 	ab aa;
 	abb bb;
+//测试模块
 //	display(aa);
 //	allDisplay(aa,bb);
-	std::cout<<aa.get(bb);
+//	std::cout<<aa.get(bb);
+	bb.getAbFormAbb(aa);
 	return 0;
 }

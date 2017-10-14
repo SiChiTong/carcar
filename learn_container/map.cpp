f0a641a79db8741683c8d86c34142f8916d2cb35
root <root@localhost.localdomain>
2017-10-13 23:17:00 -0700

    modify vritul

--- learn_container/map.cpp
+++ learn_container/map.cpp
@@ -14,10 +14,10 @@ typedef struct{
 class stu{
 	public:
 	string name ;
-	stuInfo* info=new(stuInfo);
+	stuInfo* info=new stuInfo;
 	public:
 	stu();
-	virtual ~stu(){
+        virtual ~stu(){
 		printf("delete stu object!!");
 		delete info;	
 	};
@@ -28,11 +28,12 @@ class stuMan:public stu
 {
 	private:
 	int *height=new(int);
-	
+	public:	
 	stuMan();
 	~stuMan(){
 		printf("delete stuMan !!");
 		delete height;
+		//~stu();
 		delete info;
 	};
 	void display();
@@ -48,7 +49,7 @@ stu::stu():name("okk"){
 };
 
 void stu::desplay(){
-	printf("name:%s,info->age:%d,info->_class:%s,info->phone:%s,info->email:%s,info->sort:%d"
+	printf("name:%s,info->age:%d,info->_class:%s,info->phone:%s,info->email:%s,info->sort:%d\n"
 	,name.c_str(),info->age,info->_class.c_str(),info->phone.c_str(),info->email.c_str(),info->sort);
 };
 
@@ -65,10 +66,15 @@ stuMan::stuMan(){
 void stuMan::display(){
 
 	std::cout<<"name:"<<name<<"info->age:"<<info->age<<"info->_class"<<info->_class<<"info->phone"
-	<<info->phone<<"info->email:"<<info->email<<"info->sort"<<info->sort<<"height:"<<height<<endl;
+	<<info->phone<<"info->email:"<<info->email<<"info->sort:"<<info->sort<<"height:"<<*height<<endl;
 }
 int main(void)
 {
-	stu* student1=new(stu);
+	stu* student1=new stu();
 	student1->desplay();
+	stuMan* student2=new stuMan();
+	student2->desplay();
+	delete student1;
+	//delete student2;
+
 }

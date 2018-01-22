589958dcea149a22ac80682751a9d67a63f3eea2
gongwenbo <gongwenbotarsbot@gmail.com>
2018-01-22 07:26:48 -0800

    add cmake mathod

--- BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/DependInfo.cmake
+++ BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/DependInfo.cmake
@@ -1,26 +1,26 @@
 # The set of languages for which implicit dependencies are needed:
-SET(CMAKE_DEPENDS_LANGUAGES
+set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-SET(CMAKE_DEPENDS_CHECK_CXX
-  "/root/carcar/BoostLearn2018/CommonModule/src/any.cpp" "/root/carcar/BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/any.cpp.o"
-  "/root/carcar/BoostLearn2018/CommonModule/src/array.cpp" "/root/carcar/BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/array.cpp.o"
-  "/root/carcar/BoostLearn2018/CommonModule/src/lexical.cpp" "/root/carcar/BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/lexical.cpp.o"
-  "/root/carcar/BoostLearn2018/CommonModule/src/tokenizer.cpp" "/root/carcar/BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/tokenizer.cpp.o"
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/carcar/BoostLearn2018/CommonModule/src/any.cpp" "/home/carcar/BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/any.cpp.o"
+  "/home/carcar/BoostLearn2018/CommonModule/src/array.cpp" "/home/carcar/BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/array.cpp.o"
+  "/home/carcar/BoostLearn2018/CommonModule/src/lexical.cpp" "/home/carcar/BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/lexical.cpp.o"
+  "/home/carcar/BoostLearn2018/CommonModule/src/tokenizer.cpp" "/home/carcar/BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/tokenizer.cpp.o"
   )
-SET(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Targets to which this target links.
-SET(CMAKE_TARGET_LINKED_INFO_FILES
-  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
-SET(CMAKE_C_TARGET_INCLUDE_PATH
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
   ".././inc"
   ".././src"
   "../src/../inc"
   )
-SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/cmake_clean.cmake
+++ BoostLearn2018/CommonModule/build/src/CMakeFiles/test_depend.dir/cmake_clean.cmake
@@ -1,13 +1,13 @@
-FILE(REMOVE_RECURSE
+file(REMOVE_RECURSE
   "CMakeFiles/test_depend.dir/any.cpp.o"
-  "CMakeFiles/test_depend.dir/array.cpp.o"
-  "CMakeFiles/test_depend.dir/lexical.cpp.o"
   "CMakeFiles/test_depend.dir/tokenizer.cpp.o"
+  "CMakeFiles/test_depend.dir/lexical.cpp.o"
+  "CMakeFiles/test_depend.dir/array.cpp.o"
   "libtest_depend.pdb"
   "libtest_depend.a"
 )
 
 # Per-language clean rules from dependency scanning.
-FOREACH(lang CXX)
-  INCLUDE(CMakeFiles/test_depend.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
+foreach(lang CXX)
+  include(CMakeFiles/test_depend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
